460f5de1ee88377109bd03028b20f63f4736e6da
amy-b <amy.berkhoudt@gmail.com>
2016-09-15 22:15:44 -0700

    creates html and css

---
+++ js/index.js
@@ -0,0 +1,11 @@
+//Setup: Call random cards onto each card div (81 cards total with four different variables: color - red, green, purple; number - one, two three; shape - squiggle, oval, diamond; shade - striped, solid, blank)
+
+            //Step 1: Retrieve the variables of the card one, two, and three: color, number, shade, shape
+
+            //Step 2: Compare the variables of card one, two and three
+
+            //if card one and two match with one variable, compare to card three. If card three matches on same variable tell user "correct"
+
+            //else if card one, two, and three do not match on all variables tell user "correct"
+
+            //else tell user "Incorrect"
