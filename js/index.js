130073159d6aacbdd2abd386a053a14c6ea938fc
amy-b <amy.berkhoudt@gmail.com>
2016-09-25 14:29:41 -0700

    added img of each single object

--- js/index.js
+++ js/index.js
@@ -5,8 +5,8 @@ $(document).ready(function(){
 	console.log("jQuery is up and running");
 
 	//hide instructions and examples upon loading
-		$(".instructions").hide();
-		$(".examples").hide();
+	$(".instructions").hide();
+	$(".examples").hide();
 
 	//#1 Prevent form submission 
 	event.preventDefault()
@@ -24,9 +24,118 @@ $(document).ready(function(){
 
 	function revealExamples(){
 		$(".examples").slideToggle();
-	}
+	} 
 
-})
+	var card0 = {
+		color:"red", 
+		shape:"oval", 
+		shade:"blank", 
+		number:"two"
+	};
+	var card1 = {
+		color:"green", 
+		shape:"diamond", 
+		shade:"blank", 
+		number:"two"
+	};
+	var card2 = {
+		color:"purple", 
+		shape:"diamond", 
+		shade:"blank", 
+		number:"two"
+	};
+	var card3 = {
+		color:"purple", 
+		shape:"squiggle", 
+		shade:"full", 
+		number:"three"
+	};
+	var card4 = {
+		color:"green", 
+		shape:"squiggle", 
+		shade:"blank", 
+		number:"three"
+	};
+	var card5 = {
+		color:"green", 
+		shape:"oval", 
+		shade:"blank", 
+		number:"three"
+	};
+	var card6 = {
+		color:"purple", 
+		shape:"squiggle", 
+		shade:"blank", 
+		number:"two"
+	};
+	var card7 = {
+		color:"green", 
+		shape:"diamond", 
+		shade:"stripe", 
+		number:"one"
+	};
+	var card8 = {
+		color:"green", 
+		shape:"squiggle", 
+		shade:"blank", 
+		number:"two"
+	};
+	var card9 = {
+		color:"purple", 
+		shape:"squiggle", 
+		shade:"stripe", 
+		number:"one"
+	};
+	var card10 = {
+		color:"red", 
+		shape:"diamond", 
+		shade:"full", 
+		number:"three"
+	};
+	var card11 = {
+		color:"red", 
+		shape:"diamond", 
+		shade:"blank", 
+		number:"two"
+	};
+
+	cards.push(card0);
+	cards.push(card1);
+	cards.push(card2);
+	cards.push(card3);
+	cards.push(card4);
+	cards.push(card5);
+	cards.push(card6);
+	cards.push(card7);
+	cards.push(card8);
+	cards.push(card9);
+	cards.push(card10);
+	cards.push(card11);
+	// cards.push(card5);
+	console.log(cards);
+
+cards.forEach(function(element,index){
+	}) 
+  
+  //for each card, add a set of <div>s equal to the current card's number property
+  //provide each of those new DIV's with an appropriate class based off the other propeties
+
+});
+
+//All possible values
+var options = ["red", "green", "purple", "diamond", "oval", "squiggle", "blank", "full", "stripe", "one", "two", "three"];
+
+//Here are the values in the user's hand
+var valuesInHand = ["red", "red", "circle", "diamond", "diamond", "diamond", "green"];
+
+//Let's create a string version of user's hand 
+var valuesInHandString = JSON.stringify(valuesInHand);
+console.log(valuesInHandString)
+
+//returns an array of occurences that match the text we provide
+var occurencesOfRed = valuesInHandString.match(/red/g).length;
+//alert(occurencesOfRed)
+
 
 //psuedo code the deck (6 cards to start), looping through and creating each card object on the DOM
 //push card properties onto the card div 
