dbf0715c879b829ec15be4159954dc248053596f
amy-b <amy.berkhoudt@gmail.com>
2016-09-25 09:15:46 -0700

    starts js and css

--- js/index.js
+++ js/index.js
@@ -1,11 +1,56 @@
-//Setup: Call random cards onto each card div (81 cards total with four different variables: color - red, green, purple; number - one, two three; shape - squiggle, oval, diamond; shade - striped, solid, blank)
+//Delays code from executing until all DOM assets have been loaded
+$(document).ready(function(){
 
-            //Step 1: Retrieve the variables of the card one, two, and three: color, number, shade, shape
+	//make sure js is working
+	console.log("jQuery is up and running");
 
-            //Step 2: Compare the variables of card one, two and three
+	//hide instructions and examples upon loading
+		$(".instructions").hide();
+		$(".examples").hide();
 
-            //if card one and two match with one variable, compare to card three. If card three matches on same variable tell user "correct"
+	//#1 Prevent form submission 
+	event.preventDefault()
 
-            //else if card one, two, and three do not match on all variables tell user "correct"
+	//#2 Detect when "instructions" or "examples" is clicked 
+	$(".instructions-button").click(revealInstructions);
+	$(".examples-button").click(revealExamples);
+	//of example button clicked and instructions are open, close instructions and vice versa
 
-            //else tell user "Incorrect"
+	//# Toggle instructions or examples when button is clicked
+	function revealInstructions(){
+		 console.log("reveal text");
+		 $(".instructions").slideToggle();
+	}
+
+	function revealExamples(){
+		$(".examples").slideToggle();
+	}
+
+})
+
+//psuedo code the deck (6 cards to start), looping through and creating each card object on the DOM
+//push card properties onto the card div 
+//use jQuery to display image on the cards
+//display card properties in the console
+//create a hand var currentHand=[];
+//$("div").click(function(){
+	//if (currentHand.length <3){
+		//$(this).hide();
+		//var id = $(this).attr("class");
+		//currentHand.push(cards[id]);
+		//console.log(currentHand);
+	//})
+	//else {
+		//var handArray =[];
+		//currentHand.forEach(function(obj){
+			//Object.keys(obj).map(function(key))
+			//handArray.push(obj[key]);
+
+		//})
+	//});
+	//console.log(handArray);
+	//var red = handArray.match(red);
+	//console.log("red", red)
+//}
+//});
+//conditional logic for checking if it is a set 
